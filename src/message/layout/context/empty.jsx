0d3985e8bfd8256ba8aa9962935924e62ffbfef5
warnigo <abubakirshavkatov0012@gmail.com>
2024-02-26 23:38:03 +0500

    some change message style

--- src/message/layout/context/empty.jsx
+++ src/message/layout/context/empty.jsx
@@ -3,7 +3,7 @@ import { VscEmptyWindow } from "react-icons/vsc";
 
 const EmptyMessage = () => {
     return (
-        <div className='p-6 border m-4 rounded-lg overflow-auto'>
+        <div className='p-6 border rounded-lg overflow-auto'>
             <VscEmptyWindow className='w-80 h-80 text-gray-400' />
             <h1 className='text-gray-400 text-center'>Message</h1>
         </div>
